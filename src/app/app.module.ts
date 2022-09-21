07dd3aee42dbd9cf545118230e57695bf1ffb6cb
Ngô Vưn Nam <anh02284@gmail.com>
2022-09-21 20:37:57 +0700

    feat: setting roteting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { ProductsComponent } from './pages/admin/products/products.component';
+import { NoPageComponent } from './pages/no-page/no-page.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    AboutComponent,
+    ContactsComponent,
+    SignupComponent,
+    MainLayoutComponent,
+    AdminLayoutComponent,
+    DashboardComponent,
+    ProductsComponent,
+    NoPageComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,

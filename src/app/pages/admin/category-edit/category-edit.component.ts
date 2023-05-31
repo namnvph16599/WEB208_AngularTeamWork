4ae9cc052ae560fff799279a84f446fe88fd65a1
namngodev <nam.ngo.gtsvietnam@gmail.com>
2023-05-31 16:33:25 +0700

    fix: category

--- src/app/pages/admin/category-edit/category-edit.component.ts
+++ src/app/pages/admin/category-edit/category-edit.component.ts
@@ -7,27 +7,37 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-category-edit',
   templateUrl: './category-edit.component.html',
-  styleUrls: ['./category-edit.component.css']
+  styleUrls: ['./category-edit.component.css'],
 })
 export class CategoryEditComponent implements OnInit {
-  name: String = ''
-  findCate = {}
-  id: number = 0
-  constructor(private categoryService: CategoryService, private activatedRoute: ActivatedRoute, private router: Router) { }
+  name: String = '';
+  image: String = '';
+  findCate = {};
+  id: number = 0;
+  constructor(
+    private categoryService: CategoryService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(params => {
+    this.activatedRoute.params.subscribe((params) => {
       this.id = params['id'];
-      this.categoryService.getCategory(this.id).subscribe(data => {
-        this.name = data.name
-      })
+      this.categoryService.getCategory(this.id).subscribe((data) => {
+        console.log('data', data);
+
+        this.name = data.name;
+        this.image = data.image;
+      });
     });
   }
   onHandleAdd(event: any) {
-    this.categoryService.update({ id: this.id, name: this.name }).subscribe(data => {
-      if (data) {
-        this.router.navigate(['admin/category'])
-      }
-    })
+    this.categoryService
+      .update({ id: this.id, name: this.name, image: this.image })
+      .subscribe((data) => {
+        if (data) {
+          this.router.navigate(['admin/category']);
+        }
+      });
   }
 }

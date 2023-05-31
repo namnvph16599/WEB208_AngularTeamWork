0075625dd948e1e08c82c9c9e9ff962325bb3193
namngodev <nam.ngo.gtsvietnam@gmail.com>
2023-05-31 11:40:12 +0700

    fix: main layout

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,6 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyAm-SnobSwICE1rZ2EizWOLvYwg4dbh5Pg",
-    authDomain: "angular-we17101.firebaseapp.com",
-    databaseUrl: "https://angular-we17101-default-rtdb.firebaseio.com",
-    projectId: "angular-we17101",
-    storageBucket: "angular-we17101.appspot.com",
-    messagingSenderId: "235805532535",
-    appId: "1:235805532535:web:a8e799e0d36c933551c172",
-    measurementId: "G-VEM31Y5B22"
-  }
 };
 
 /*

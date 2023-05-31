2af7d476a590f4e165c1937dddeb9c52413cea5a
namngodev <nam.ngo.gtsvietnam@gmail.com>
2023-05-31 16:00:28 +0700

    fix: navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
+  user: any = {};
 
   ngOnInit(): void {
+    const userInfo = localStorage.getItem('user');
+    if (userInfo) {
+      this.user = JSON.parse(userInfo);
+      console.log('this.user', this.user);
+    }
   }
-
 }

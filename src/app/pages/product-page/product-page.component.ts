40fabc717b1ab6c2d9fd8627446c302909b18a83
Ngô Vưn Nam <anh02284@gmail.com>
2023-06-10 00:00:20 +0700

    feat: search

--- src/app/pages/product-page/product-page.component.ts
+++ src/app/pages/product-page/product-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Event, Router, RouterEvent } from '@angular/router';
+import { filter } from 'rxjs';
 import { IProduct, Category } from 'src/app/interfaces';
 import { CategoryService } from 'src/app/services/category.service';
 import { ProductService } from 'src/app/services/product.service';
@@ -10,14 +11,31 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./product-page.component.css'],
 })
 export class ProductPageComponent implements OnInit {
+  search: string = '';
+  categories: Category[] = [];
+  products: IProduct[] = [];
+
   constructor(
     private categoryService: CategoryService,
     private productService: ProductService,
-    private route: ActivatedRoute
-  ) {}
-
-  categories: Category[] = [];
-  products: IProduct[] = [];
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    router.events
+      .pipe(filter((e: Event): e is RouterEvent => e instanceof RouterEvent))
+      .subscribe((e: RouterEvent) => {
+        // handle search
+        const isSearch = e.url.indexOf('search');
+        if (isSearch < 0) return;
+        const valueSearch = e.url.slice(16);
+        if (this.search !== valueSearch) {
+          this.productService.searchProduct(valueSearch).subscribe((pros) => {
+            this.products = pros;
+            this.search = valueSearch;
+          });
+        }
+      });
+  }
 
   ngOnInit(): void {
     this.categoryService.getCategories().subscribe((data) => {
@@ -32,9 +50,11 @@ export class ProductPageComponent implements OnInit {
         });
         return;
       }
-      this.productService.getProducts().subscribe((pros) => {
-        this.products = pros;
-      });
+      if (!this.search) {
+        this.productService.getProducts().subscribe((pros) => {
+          this.products = pros;
+        });
+      }
     });
   }
 }
